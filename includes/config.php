81a39f6ec081843ac2cfcc64f3ab17a869d277bb
Rajitha Perera <rajiteh@gmail.com>
2015-03-03 14:40:10 -0500

    Configure for production

--- includes/config.php
+++ includes/config.php
@@ -4,11 +4,13 @@ class Config {
 	const DATABASE_STRING = 'mysql:host=localhost;dbname=botb;charset=utf8',
 		  DATABASE_USER = 'botb',
 		  DATABASE_PASSWORD = 'botb',
-		  DEBUG = '1',
+		  DEBUG = '0',
 		  REDIS_DSN = 'redis://localhost:6379/0',
           CACHE_TTL = 30,
           SUBSCRIBER_TTL = 60,
-		  PUSHWOOSH_APP = '***REMOVED***',
-		  PUSHWOOSH_TOKEN = '***REMOVED***';
+          PUSHWOOSH_APP = '***REMOVED***',
+          PUSHWOOSH_TOKEN = '***REMOVED***';
+		  //PUSHWOOSH_APP = '***REMOVED***',
+          //PUSHWOOSH_TOKEN = '***REMOVED***';
 
 }
